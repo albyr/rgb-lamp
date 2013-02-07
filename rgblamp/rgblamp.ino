173e7ede8d5ab65cb3b5a23f60ddb68b71429355
Alby Reid <alby@bleary-id.co.uk>
2013-02-07 22:53:24 +0000

    Begin making things actually work
    Added debugging code and improved commenting.

--- rgblamp/rgblamp.ino
+++ rgblamp/rgblamp.ino
@@ -1,4 +1,5 @@
 // define output LEDs
+// using pins 9, 10 and 11 because they are PWM pins
 #define rLED 9
 #define gLED 10
 #define bLED 11
@@ -14,12 +15,17 @@ int bBright=0;
 int rMax=256;
 int gMax=256;
 int bMax=256;
+// Fade rate control
+int fadeDelay=10;
 
 void setup()
 {
+  // Set up pins
   pinMode(rLED, OUTPUT);
   pinMode(gLED, OUTPUT);
   pinMode(bLED, OUTPUT);
+  // Set up serial output for debugging
+  Serial.begin(9600);
 }
 
 void loop()
@@ -28,40 +34,48 @@ void loop()
   rBright = random(rMax);
   gBright = random(gMax);
   bBright = random(bMax);
+  // Output RGB values for debugging
+  Serial.println("R:");
+  Serial.println(rBright);
+  Serial.println("G:");
+  Serial.println(gBright);
+  Serial.println("B:");
+  Serial.println(bBright);
+  Serial.println("---");
   // Fade in red
-  for (i=0; i<255; i++)
+  for (i=0; i<rBright; i++)
   {
     analogWrite(rLED, i);
-    delay(10);
-  }
-  // Fade out red
-  for (i=255; i>0; i--)
-  {
-    analogWrite(rLED, i);
-    delay(10);
+    delay(fadeDelay);
   }
   // Fade in green
-  for (i=0; i<255; i++)
+  for (i=0; i<gBright; i++)
   {
     analogWrite(gLED, i);
-    delay(10);
-  }
-  // Fade out green
-  for (i=255; i>0; i--)
-  {
-    analogWrite(gLED, i);
-    delay(10);
+    delay(fadeDelay);
   }
   // Fade in blue
-  for (i=0; i<255; i++)
+  for (i=0; i<bBright; i++)
   {
     analogWrite(bLED, i);
-    delay(10);
+    delay(fadeDelay);
+  }
+  // Fade out red
+  for (i=rBright; i>0; i--)
+  {
+    analogWrite(rLED, i);
+    delay(fadeDelay);
+  }
+  // Fade out green
+  for (i=gBright; i>0; i--)
+  {
+    analogWrite(gLED, i);
+    delay(fadeDelay);
   }
   // Fade out blue
-  for (i=255; i>0; i--)
+  for (i=bBright; i>0; i--)
   {
     analogWrite(bLED, i);
-    delay(10);
+    delay(fadeDelay);
   }
 }
